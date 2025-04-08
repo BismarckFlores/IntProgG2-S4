529ecdcb5aea1336b8b5c45db88863bd41542e46
Bismarck Flores <bjflores@uamv.edu.ni>
2025-04-08 10:28:25 -0600

    Ejercicio 1

--- Sesion_3/Ejercicio1.py
+++ Sesion_3/Ejercicio1.py
@@ -2,4 +2,4 @@ nombre = "Bismarck"
 edad = 17
 estatura = 1.79
 
-print(f'{"Nombre:":<10} {nombre:<8} \n{"Edad":<10} {edad:<8} \n{"Estatura":<10} {estatura:<8}')
+print(f'{"Nombre:":<10} {nombre:<8} \n{"Edad:":<10} {edad:<8} \n{"Estatura:":<10} {estatura:<8}')
