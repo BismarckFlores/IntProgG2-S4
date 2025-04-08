14d5c690d3083c38b57854b34f2c91239a3a9571
Bismarck Flores <bjflores@uamv.edu.ni>
2025-04-08 11:38:46 -0600

    Ejercicio 10

---
+++ Sesion_2/Ejercicio10.py
@@ -0,0 +1,13 @@
+"""Declara nombres de variables para los siguientes casos, aplicando la convención
+indicada.
+1. Nombre completo del estudiante – camelCase
+2. Número de matrícula – snake_case
+3. Total de ventas del mes – PascalCase
+4. Límite máximo de intentos – SCREAMING_SNAKE_CASE
+5. Cantidad de productos – camelCase"""
+
+print("1. Nombre completo del estudiante – camelCase → nombreCompletoEstudiante")
+print("2. Número de matrícula – snake_case → numero_de_matricula")
+print("3. Total de ventas del mes – PascalCase → TotalDeVentasDelMes")
+print("4. Límite máximo de intentos – SCREAMING_SNAKE_CASE → LIMITE_MAXIMO_DE_INTENTOS")
+print("5. Cantidad de productos – camelCase → cantidadDeProductos")
