c17478396c99ab3b77fe8e93974f3aa2acecb212
Bismarck Flores <bjflores@uamv.edu.ni>
2025-04-08 11:38:56 -0600

    Ejercicio 11

---
+++ Sesion_2/Ejercicio11.py
@@ -0,0 +1,16 @@
+"""A partir de las siguientes descripciones, escribe el nombre de una variable que siga
+una convención lógica y clara. Puedes elegir la convención, pero justificarla."""
+
+a = "La edad del usuario"
+b = "El precio total del carrito de compras"
+c = "El estado de inicio de sesión (activo o inactivo)"
+d = "El nombre del curso actual"
+e = "La temperatura promedio registrada"
+
+print(f'{a} → edadUsuario')
+print(f'{b} → precioTotalCarrito')
+print(f'{c} → sesionActiva')
+print(f'{d} → nombreCursoActual')
+print(f'{e} → temperaturaPromedio')
+
+print("Utilize camelCase devido a que es una nomenclatura con la que ya estoy familiarizado y me resulta mas facil de usar")
