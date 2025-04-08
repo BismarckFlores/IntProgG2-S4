301aafbe8d043e11c6c64f724f626be57396ea0b
Bismarck Flores <bjflores@uamv.edu.ni>
2025-04-08 17:25:05 -0600

    Ejercicio 14

--- Sesion_2/Ejercicio14.py
+++ Sesion_2/Ejercicio14.py
@@ -4,12 +4,12 @@ salario_actual = float(input("Ingrese su salario actual: "))
 incremento = salario_actual * 0.08
 salario_incrementado = salario_actual + incremento
 descuento_servicios = salario_incrementado * 0.025
-nuevo_salario = salario_incrementado + descuento_servicios
+nuevo_salario = salario_incrementado - descuento_servicios
 
 print("\n")
 print("=" * 19, "Resultados", "=" * 19)
-print(f'{"Salario actual:":<36} ${salario_actual:>12}')
-print(f'{"Incremento (8%):":<36} ${incremento:>12}')
-print(f'{"Descuento por servicios (2.5%):":<36} ${descuento_servicios:>12}')
-print(f'{"Nuevo salario:":<36} ${nuevo_salario:>12}')
+print(f'{"Salario actual:":<36} ${salario_actual:>12,.2f}')
+print(f'{"Incremento (8%):":<36} ${incremento:>12,.2f}')
+print(f'{"Descuento por servicios (2.5%):":<36} ${descuento_servicios:>12,.2f}')
+print(f'{"Nuevo salario:":<36} ${nuevo_salario:>12,.2f}')
 print("="*50)
