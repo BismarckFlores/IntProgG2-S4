6fb71f042b56a416067d95640777e84ea9e6b959
Bismarck Flores <bjflores@uamv.edu.ni>
2025-04-08 08:41:06 -0600

    Primera clase miercoles 08/04/25

---
+++ ejercicio1.py
@@ -0,0 +1,10 @@
+#Declarar variables
+nombre_completo = "Jose Cristo Carvallo Herrera"
+edad = 17
+altura = 1.72
+soltero = True
+
+print(f'Nombre Completo: {nombre_completo}\nTipo de dato: {type(nombre_completo)}')
+print(f'Edad: {edad}\nTipo de dato: {type(edad)}')
+print(f'Altura: {altura: .2f}\nTipo de dato: {type(altura)}')
+print(f'Estado civil: {soltero}\nTipo de dato: {type(soltero)}')
