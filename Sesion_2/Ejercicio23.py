fb8465033698b40f6a66c6b319e5212357cfbaf3
Bismarck Flores <bjflores@uamv.edu.ni>
2025-04-08 18:50:30 -0600

    Ejercicio 23

--- Sesion_2/Ejercicio23.py
+++ Sesion_2/Ejercicio23.py
@@ -5,5 +5,7 @@ ganancia = precio_compra * 0.30
 precio_venta = precio_compra + ganancia
 
 print("=" * 19, "Resultados", "=" * 19, "\n")
+print(f'{"Precio de compra:":<36} ${precio_compra:>12,.2f}')
+print(f'{"Ganancia (30%):":<36} ${ganancia:>12,.2f}')
 print(f'{"Precio de venta:":<36} ${precio_venta:>12,.2f}')
 print("=" * 50)
