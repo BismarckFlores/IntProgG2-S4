5d81f67d95453c143528eda0f81a8f279b8bdaa9
Bismarck Flores <bjflores@uamv.edu.ni>
2025-04-08 18:49:40 -0600

    Ejercicio 23

---
+++ Sesion_2/Ejercicio23.py
@@ -0,0 +1,9 @@
+print("=" * 11, "Cálculo de precio de venta", "=" * 11, "\n")
+precio_compra = float(input("Ingrese el precio de compra: $"))
+
+ganancia = precio_compra * 0.30
+precio_venta = precio_compra + ganancia
+
+print("=" * 19, "Resultados", "=" * 19, "\n")
+print(f'{"Precio de venta:":<36} ${precio_venta:>12,.2f}')
+print("=" * 50)
